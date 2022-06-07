0ea3fcd319127752e57245b15bdfacf15e38a3b4
Christian <chrbudtz@gmail.com>
2022-06-07 19:10:48 +0200

    with realtime sockets

--- api/src/index.js
+++ api/src/index.js
@@ -1,23 +1,41 @@
 import express from 'express';
 import cors from 'cors';
+import {Server} from 'socket.io';
+import {createServer} from 'http';
 
 console.log("Starting server - mode: " + process.env.NODE_ENV);
+
+let chat="";
 
 const app = express();
 const PORT = 5000;
 app.use(cors());
 
-
-
 app.get("/api",(req,res)=>{
     res.send("API time is " + new Date())
 })
 
+const socketServer = createServer(app);
+const sockIo = new Server(socketServer,{
+    cors:"*"
+});
 
+sockIo.on('connection',(socket)=>{
+    console.log("User connected! :");
+    socket.on('msg', (message)=>{
+        chat+="\r\n" + socket.id + ": " + message;
+        console.log("chatState: " + chat);
+        sockIo.emit("chat", chat)
+
+    })
+    socket.on('disconnect',()=>{
+        console.log("User disconnected!");
+    })
+})
 
 app.use(express.static('src/public'));
 
-app.listen(PORT, ()=>{
+socketServer.listen(PORT, ()=>{
     console.log("Server running on: " + PORT);
 });
 
--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import logo from './logo.svg';
 import './App.scss';
 import React, {useEffect, useState} from 'react'
+import socketIOClient from 'socket.io-client';
 import Frontpage from './Pages/Frontpage/Frontpage.jsx';
 import Inputpage from './Pages/Inputpage/Inputpage.jsx';
 
-const BASEURL = process.env.NODE_ENV=== "development" ? "http://localhost:5000/api" : "/api"
+const BASEURL = process.env.NODE_ENV=== "development" ? "http://localhost:5000" : ""
+const BASEAPI = BASEURL + "/api"
+let socket; //Should be centralized to a statemanager
 
 function App(props) {
   
   const [userID, setUserID] = useState(null);
   const [backend, setBackend] = useState(null);
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState("chat");
 
   const callBackend = async ()=>{
-    const res = await fetch(BASEURL);
+    const res = await fetch(BASEAPI);
     const text = await res.text();
     setBackend(text);
+    socket = socketIOClient(BASEURL);
+    console.log(socket)
+    socket.on("chat",(remoteChat)=>{
+      setChat(remoteChat);
+    })
+  }
+
+  const sendMessage = ()=>{
+    socket.emit('msg',message);
+    setMessage("");
   }
 
   return (
@@ -23,9 +38,14 @@ function App(props) {
           {props.page === "Inputpage" ? <Inputpage userID={userID}/> : null}
           {props.page === "Frontpage" ? <Frontpage userID={userID}/> : null}
       </div>
-      <button onClick={callBackend}>Test backend</button>
-      {backend && <div> Backend says : {backend}
+      <button onClick={callBackend}>Test backend - make connection</button>
+      {backend && <>
+        <div> Backend says : {backend}
         </div>
+        <pre>{chat} </pre>
+        <input type="text" value={message} onChange={(e)=>setMessage(e.target.value)}></input>
+        <button onClick={sendMessage}> send message</button>
+        </>
         }
 
     </div>
