2fdfbbb264e4f3b67f056b9fa6728c43cd04b164
chbu <chbu@dtu.dk>
2022-06-07 15:41:09 +0200

    express init

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,17 @@
-import express, { application } from 'express';
+import express from 'express';
 
 console.log("Starting server");
 
 const app = express();
-const 
+const PORT = 3000;
 
-app.listen(3000);
+app.get("/api",(req,res)=>{
+    res.send("API")
+})
+app.use(express.static('src/public'));
+
+app.listen(PORT, ()=>{
+    console.log("Server running on: " + PORT);
+});
 
 
